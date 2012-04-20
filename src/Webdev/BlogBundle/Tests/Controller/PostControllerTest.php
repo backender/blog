c5065e392430bfabf2cdd4c6eb9e6b4088b27c7b
Marc Juchli <marc@mjnet.ch>
2012-04-20 14:58:30 +0200

    unit test sample
    have to try it on anoter machine or intall unittest on mac

--- src/Webdev/BlogBundle/Tests/Controller/PostControllerTest.php
+++ src/Webdev/BlogBundle/Tests/Controller/PostControllerTest.php
@@ -14,4 +14,13 @@ class PostControllerTest extends WebTestCase
 
         $this->assertTrue($crawler->filter('h2:contains("symfony2 Tutorial")')->count() > 0);
     }
+    
+    public function testPostLoader()
+    {
+    	$client = static::createClient();
+    	
+        $crawler = $client->request('GET', '/post/symfony2-tutorial');
+    	    	
+    	$this->assertEquals($crawler->filterXPath('/html/body/div/div[2]/ul/li/a')->text(), 'symfony 2');
+    }
 }
