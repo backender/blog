1b9b7fcf1919ef30713ad16bf9a370e2398ab0fb
Marc Juchli <marc@mjnet.ch>
2012-04-21 18:29:21 +0200

    Separate Post/Tag fixtures

--- src/Webdev/BlogBundle/DataFixtures/ORM/LoadPostData.php
+++ src/Webdev/BlogBundle/DataFixtures/ORM/LoadPostData.php
@@ -1,41 +1,47 @@
 <?php
 namespace Webdev\BlogBundle\DateFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Webdev\BlogBundle\Entity\Post;
 use Webdev\BlogBundle\Entity\Tag;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadPostData implements FixtureInterface
+class LoadPostData extends AbstractFixture implements OrderedFixtureInterface
 {
+	public function getOrder()
+	{
+		return 2;
+	}
+	
 	public function load(ObjectManager $manager)
 	{
+		$post1 = new Post();
+		$post1->setTitle('symfony2 Tutorial');
+		$post1->setSlug('symfony2-tutorial');
+		$post1->setContent('<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>');
+		$post1->addTag($this->getReference('tag1'));
+		$post1->addTag($this->getReference('tag2'));
+		$post1->addTag($this->getReference('tag3'));
+		
+		$post2 = new Post();
+		$post2->setTitle('second symfony post');
+		$post2->setSlug('second-symfony-post');
+		$post2->setContent('<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>');
+		$post2->addTag($this->getReference('tag1'));
+		$post2->addTag($this->getReference('tag2'));
+		//$post2->addTag($this->getReference('tag3'));
+		
+		$manager->persist($post1);
+		$manager->persist($post2);
 
-		$tag1 = new Tag();
-		$tag1->setName('symfony 2');
-		$tag1->setQuantifier(3);
-		
-		$tag2 = new Tag();
-		$tag2->setName('Tutorial');
-		$tag2->setQuantifier(1);
-		
-		$tag3 = new Tag();
-		$tag3->setName('PHP');
-		$tag3->setQuantifier(6);
-		
-		$post = new Post();
-		$post->setTitle('symfony2 Tutorial');
-		$post->setSlug('symfony2-tutorial');
-		$post->setContent('<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>');
-		$post->addTag($tag1);
-		$post->addTag($tag2);
-		$post->addTag($tag3);
-		
-		$manager->persist($post);
-		$manager->persist($tag1);
-		$manager->persist($tag2);
-		$manager->persist($tag3);
 		$manager->flush();
+		
+		$this->addReference('post1', $post1);
+		$this->addReference('post2', $post2);
 	}
 }
---
+++ src/Webdev/BlogBundle/DataFixtures/ORM/LoadTagData.php
@@ -0,0 +1,45 @@
+<?php
+namespace Webdev\BlogBundle\DateFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Webdev\BlogBundle\Entity\Post;
+use Webdev\BlogBundle\Entity\Tag;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadTagData extends AbstractFixture implements OrderedFixtureInterface
+{
+	public function getOrder()
+	{
+		return 1;
+	}
+	
+	public function load(ObjectManager $manager)
+	{
+		$tag1 = new Tag();
+		$tag1->setName('symfony 2');
+		$tag1->setQuantifier(3);
+		
+		$tag2 = new Tag();
+		$tag2->setName('Tutorial');
+		$tag2->setQuantifier(1);
+		
+		$tag3 = new Tag();
+		$tag3->setName('PHP');
+		$tag3->setQuantifier(6);
+		
+		$manager->persist($tag1);
+		$manager->persist($tag2);
+		$manager->persist($tag3);
+		
+		$manager->flush();
+		
+		$this->addReference('tag1', $tag1);
+		$this->addReference('tag2', $tag2);
+		$this->addReference('tag3', $tag3);
+	}
+}
