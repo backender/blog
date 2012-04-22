2830e5f8db5c21f4babfec723d787fb8aad3b80e
Marc Juchli <marc@mjnet.ch>
2012-04-22 17:18:56 +0200

    AppBundle with entity authorisation including users and roles

---
+++ src/Webdev/AppBundle/DataFixtures/ORM/LoadRoleData.php
@@ -0,0 +1,34 @@
+<?php
+namespace Webdev\AppBundle\DateFixtures\ORM;
+
+use Webdev\AppBundle\Entity\Role;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadRoleData extends AbstractFixture implements OrderedFixtureInterface
+{
+	public function getOrder()
+	{
+		return 2;
+	}
+	
+	public function load(ObjectManager $manager)
+	{
+		$role1 = new Role();
+		$role1->setName('ROLE_USER');
+		
+		$role2 = new Role();
+		$role2->setName('ROLE_ADMIN');
+		
+		$manager->persist($role1);
+		$manager->persist($role2);
+
+		$manager->flush();
+		
+		$this->addReference('role1', $role1);
+		$this->addReference('role2', $role2);
+	}
+}
