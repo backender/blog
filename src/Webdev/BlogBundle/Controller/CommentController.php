81abffb62dc756ecc462d958c20a3a77f7701739
Marc Juchli <marc@mjnet.ch>
2012-04-26 22:42:30 +0200

    fixed the issue of missing argument after forum submit. see forum post (last commit)

--- src/Webdev/BlogBundle/Controller/CommentController.php
+++ src/Webdev/BlogBundle/Controller/CommentController.php
@@ -18,11 +18,12 @@ use Symfony\Component\HttpFoundation\Response;
 class CommentController extends Controller
 {    
 	/**
-	 * @Route("/post", name="newComment")
-     * @Template()
+	 * @Route("/post/{slug}/comment", name="post_newComment")
      */
-	public function newCommentAction(Request $request, $slug = 0)
+	public function newCommentAction($slug)
 	{
+		$request = $this->getRequest();
+		
 		$em = $this->getDoctrine()->getEntityManager();
 		$post = $em->getRepository('WebdevBlogBundle:Post')->findOneBySlug($slug);
 		
@@ -39,7 +40,6 @@ class CommentController extends Controller
 		$validator = $this->get('validator');
 		$errors = $validator->validate($comment);
 		
-		
 		if ($request->getMethod() == 'POST') {
 			$form->bindRequest($request);
 		
@@ -47,7 +47,7 @@ class CommentController extends Controller
 				$em = $this->getDoctrine()->getEntityManager();
 				$em->persist($comment);
 				$em->flush();
-				return $this->redirect($this->generateUrl("/post/".$slug));
+				return $this->redirect($this->generateUrl("blog_post_view", array('slug' => $slug)));
 			}
 		}
 		return $this->render('WebdevBlogBundle:Comment:newcomment.html.twig', array('newcomment' => $form->createView(), 'slug' => $slug));
