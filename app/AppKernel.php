e2a783ded73a710376a4f8c11ee8cf35ad5ba4a4
backender <marc@mjnet.ch>
2013-01-08 23:31:04 +0100

    knpmarkdown

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
         	new Knp\Bundle\DisqusBundle\KnpDisqusBundle(),
         	new Eko\FeedBundle\EkoFeedBundle(),
             new Liip\ThemeBundle\LiipThemeBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
